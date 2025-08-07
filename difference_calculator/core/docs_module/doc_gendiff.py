00af8ead46a856807c41ba4c20cbed2c39ac6f95
Velsych <velsych@mail.ru>
2025-08-08 00:08:40 +0300

    fixed initial script work

--- difference_calculator/core/docs_module/doc_gendiff.py
+++ difference_calculator/core/docs_module/doc_gendiff.py
@@ -5,6 +5,6 @@ def doc_start():
         prog= "gendiff",
         description= "Compares two configuration files and shows a difference."
     )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
-    return parser.print_help()
+    parser.add_argument("first_file",default=argparse.SUPPRESS)
+    parser.add_argument("second_file",default=argparse.SUPPRESS)
+    return parser.parse_args()
