94d694e411baf9fc395e7706ac47be3f312db7bf
Velsych <velsych@mail.ru>
2025-09-04 02:35:40 +0300

    fixed ruff checks in whole package

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,94 +1,105 @@
-import gendiff
 import pytest
+
+import gendiff
 
 
 @pytest.fixture
 def stylish_correct_flat():
-    with open("tests/test_data/flat_data.txt","r") as f:
+    with open("tests/test_data/flat_data.txt", "r") as f:
         stylish = f.read()
     return stylish
 
+
 @pytest.fixture
 def stylish_correct_not_flat():
-    with open("tests/test_data/not_flat_data.txt","r") as f:
+    with open("tests/test_data/not_flat_data.txt", "r") as f:
         stylish_not_flat = f.read()
     return stylish_not_flat
 
+
 @pytest.fixture
 def plain_correct_flat():
-    with open("tests/test_data/plain_flat.txt","r") as f:
+    with open("tests/test_data/plain_flat.txt", "r") as f:
         plain = f.read()
     return plain
 
+
 @pytest.fixture
 def plain_correct():
-    with open("tests/test_data/plain_not_flat.txt","r") as f:
+    with open("tests/test_data/plain_not_flat.txt", "r") as f:
         plain_flat = f.read()
     return plain_flat
 
 
 @pytest.fixture
 def json_flat_correct():
-    with open("tests/test_data/true_json_flat.json","r") as f:
+    with open("tests/test_data/true_json_flat.json", "r") as f:
         json_flat = f.read()
     return json_flat
 
+
 @pytest.fixture
 def json_not_flat_correct():
-    with open("tests/test_data/true_json.json","r") as f:
+    with open("tests/test_data/true_json.json", "r") as f:
         json = f.read()
     return json
-
 
 
 @pytest.fixture
 def file1_flat():
     return "tests/file1.json"
 
+
 @pytest.fixture
 def file2_flat():
     return "tests/file2.yaml"
 
+
 @pytest.fixture
 def file1_not_flat():
     return "tests/not_flat.json"
+
 
 @pytest.fixture
 def file2_not_flat():
     return "tests/not_flat2.json"
 
 
-
-
-
-
-def test_stylish_flat(file1_flat,file2_flat,stylish_correct_flat):
-    formatted = gendiff.diff_generator.generate_diff(file1_flat,file2_flat,"stylish")
+def test_stylish_flat(file1_flat, file2_flat, stylish_correct_flat):
+    formatted = gendiff.diff_generator.generate_diff(file1_flat,
+                                                      file2_flat, "stylish")
     assert formatted == stylish_correct_flat
 
 
-
-def test_stylish_not_flat(file1_not_flat,file2_not_flat,stylish_correct_not_flat):
-    formatted = gendiff.diff_generator.generate_diff(file1_not_flat,file2_not_flat,"stylish")
+def test_stylish_not_flat(file1_not_flat, file2_not_flat,
+                           stylish_correct_not_flat):
+    formatted = gendiff.diff_generator.generate_diff(file1_not_flat,
+                                                      file2_not_flat, "stylish")
     assert formatted == stylish_correct_not_flat
 
 
-def test_plain_not_flat(file1_not_flat,file2_not_flat,plain_correct):
-    formatted = gendiff.diff_generator.generate_diff(file1_not_flat,file2_not_flat,"plain")
+def test_plain_not_flat(file1_not_flat,
+                         file2_not_flat, plain_correct):
+    formatted = gendiff.diff_generator.generate_diff(file1_not_flat,
+                                                      file2_not_flat, "plain")
     assert formatted == plain_correct
 
 
-def test_plain_flat(file1_flat,file2_flat,plain_correct_flat):
-    formatted = gendiff.diff_generator.generate_diff(file1_flat,file2_flat,"plain")
+def test_plain_flat(file1_flat, file2_flat, plain_correct_flat):
+    formatted = gendiff.diff_generator.generate_diff(file1_flat,
+                                                      file2_flat, "plain")
     assert formatted == plain_correct_flat
 
 
-def test_json_flat(file1_flat,file2_flat,json_flat_correct):
-    formatted = gendiff.diff_generator.generate_diff(file1_flat,file2_flat,"json")
+def test_json_flat(file1_flat, file2_flat, json_flat_correct):
+    formatted = gendiff.diff_generator.generate_diff(file1_flat,
+                                                      file2_flat, "json")
     assert formatted == json_flat_correct
 
-def test_json_not_flat(file1_not_flat,file2_not_flat,json_not_flat_correct):
-    formatted = gendiff.diff_generator.generate_diff(file1_not_flat,file2_not_flat,"json")
+
+def test_json_not_flat(file1_not_flat, file2_not_flat, json_not_flat_correct):
+    formatted = gendiff.diff_generator.generate_diff(file1_not_flat,
+                                                      file2_not_flat, "json")
     assert formatted == json_not_flat_correct
 
 
